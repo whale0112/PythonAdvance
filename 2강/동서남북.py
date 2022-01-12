123c256377d838c0ac9929a66bf2b5a37bf59dd3
whale0112 <91520694+whale0112@users.noreply.github.com>
2022-01-12 15:42:29 +0900

    Create 동서남북.py

---
+++ 2강/동서남북.py
@@ -0,0 +1,30 @@
+from turtle import *
+t1 = Turtle()
+t1.shape("turtle")
+t1.color("orange")
+t1.seth(90)
+t1.forward(100)
+t1.write("90     ", left)
+
+t2 = Turtle()
+t2.shape("turtle")
+t2.color("orange")
+t2.seth(180)
+t2.forward(100)
+t2.write("180     ", left)
+
+t3 = Turtle()
+t3.shape("turtle")
+t3.color("orange")
+t3.seth(270)
+t3.forward(100)
+t3.write("270     ", left)
+
+t4 = Turtle()
+t4.shape("turtle")
+t4.color("orange")
+t4.seth(0)
+t4.forward(100)
+t4.write("0     ", left)
+
+done()
