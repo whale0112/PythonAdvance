83f4f05dd3cf21b25bdec1ccfbfa6a62aa5f53c5
whale0112 <91520694+whale0112@users.noreply.github.com>
2022-01-12 15:41:51 +0900

    Create main3.py

---
+++ 2강/main3.py
@@ -0,0 +1,11 @@
+import turtle
+for i in range(20):
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(72)
+turtle.mainloop()
