3ddb2df51931e250c49d47200bcaa2bae80a91d8
whale0112 <91520694+whale0112@users.noreply.github.com>
2022-10-12 17:57:01 +0900

    Create 문제7.py

---
+++ 22강/문제7.py
@@ -0,0 +1,17 @@
+def solution(k):
+    answer = []
+    for i in range(1, k + 1):
+        square_num = i * i
+        divisor = 1
+        while square_num > divisor:
+            front = square_num // divisor
+            back = square_num % divisor
+            divisor *= 10
+            if back != 0 and front != 0:
+                if front + back == i:
+                    answer.append(i)
+    return answer
+
+k = 500
+ret = solution(k)
+print("solution 함수의 반환 값은 ", ret, " 입니다.")
